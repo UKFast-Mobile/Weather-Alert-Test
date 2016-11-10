30d03c4bc081320ab4861cbd5d6f93cd456121d2
Aleksandr Kelbas <aleksandr.kelbas@ukfast.co.uk>
2016-11-10 17:02:48 +0000

    Core Data Controller setup

---
+++ Weather Alert/CoreData/City+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  City+CoreDataProperties.swift
+//  Weather Alert
+//
+//  Created by Aleksandr Kelbas on 10/11/2016.
+//  Copyright © 2016 UKFast. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension City {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<City> {
+        return NSFetchRequest<City>(entityName: "City");
+    }
+
+
+}
