1c9a3e4fafd94d527444f36ffb8a65b1cd6bd7a1
Jack Hopkins <jack.hoptz@gmail.com>
2016-11-24 17:26:09 +0000

    Tempory commit

--- Weather Alert/CItyTableViewController.swift
+++ Weather Alert/CItyTableViewController.swift
@@ -12,25 +12,30 @@ import CoreData
 class CityTableViewController: UITableViewController {
     
     // MARK: Variables
-    
+        
     var dataController: DataController { return AppShared.instances.dataController }
-    
     var cities: [City] {
-        
         let citiesFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "City")
-        
-        do {
-            return try dataController.managedObjectContext.fetch(citiesFetch) as! [City]
-        } catch {
-            fatalError("Failed to fetch employees: \(error)")
-        }
+        do { return try dataController.managedObjectContext.fetch(citiesFetch) as! [City] }
+        catch { fatalError("Failed to fetch employees: \(error)") }
     }
+    
+    // MARK: UISearchController
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var filterCities: [City] = []
     
     
     // MARK: Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchController.searchResultsUpdater = self as UISearchResultsUpdating
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
     }
     
     @IBAction func searchEditingChanged(_ sender: UITextField) {
@@ -45,46 +50,65 @@ class CityTableViewController: UITableViewController {
     // MARK: Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (section == 1 ? cities.count : 1 )
+        return (activeSearch ? filterCities : cities ).count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
         
-        print(indexPath.section)
+        let cellIdentifier = "CityTableViewCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! CityTableViewCell
+        let city = (activeSearch ? filterCities : cities )[indexPath.row]
         
-        if indexPath.section == 0 {
-            let cellIdentifier = "SearchTableViewCell"
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! SearchTableViewCell
-            return cell
+        
+        cell.directionLabel.text = city.direction
+        cell.nameLabel.text = city.name
+        cell.countryLabel.text = city.country
+        
+        if let degree = city.deg?.stringValue {
+            cell.degLabel.text = degree + "°"
+        } else {
+            cell.degLabel.text = "Unknown"
         }
-        else {
-            let cellIdentifier = "CityTableViewCell"
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! CityTableViewCell
-            
-            let city = cities[indexPath.row]
-            
-            cell.directionLabel.text = city.direction
-            cell.nameLabel.text = city.name
-            cell.countryLabel.text = city.country
-            
-            if let degree = city.deg?.stringValue {
-                cell.degLabel.text = degree + "°"
-            } else {
-                cell.degLabel.text = "Unknown"
-            }
-            
-            if let spd = city.speed?.stringValue {
-                cell.speedLabel.text = spd + " mph"
-            } else {
-                cell.speedLabel.text = "Unknown"
-            }
-            
-            return cell
+        
+        if let spd = city.speed?.stringValue {
+            cell.speedLabel.text = spd + " mph"
+        } else {
+            cell.speedLabel.text = "Unknown"
+        }
+        
+        return cell
+    }
+}
+
+extension CityTableViewController: UISearchResultsUpdating {
+    
+    var activeSearch: Bool {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return true
+        } else {
+            return false
         }
     }
+    
+    func filterContentForSearchText(searchText: String?, scope: String = "All") {
+        filterCities = cities.filter { city in
+            if let cityName = city.name, let lowerSearch = searchText?.lowercased() {
+                return cityName.lowercased().contains(lowerSearch)
+            }
+            else {
+                return false
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text)
+    }
+    
 }
